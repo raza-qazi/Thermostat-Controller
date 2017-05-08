5a6be8e52e02832582fa4bc86e46a2477e8a2c9f
tartgold <rmqazi@hotmail.com>
2017-05-07 18:22:10 -0600

    Added a button stub for testing purposes and a simple implementation for a global variable "temperature"

--- app/views.py
+++ app/views.py
@@ -1,27 +1,37 @@
 from app import app
-from flask import Flask, redirect, url_for, request, render_template, jsonify
+from flask import request, render_template, jsonify
 import datetime
+
+temperature = 1
 
 
 @app.route('/', methods=['POST', 'GET'])
 @app.route('/index', methods=['POST', 'GET'])
 def index():
+    global temperature
     if request.method == 'POST':
-        user = request.form['formbox']
+        box = request.form['formbox']
         return render_template('index.html',
-                               title='Thermostat', user="Sir", form=user)
+                               title='Thermostat', form=box, temp=temperature)
     elif request.method == 'GET':
         return render_template('index.html',
-                               title='Thermostat', user="Sir")
+                               title='Thermostat', temp=temperature)
 
 
 @app.route('/time')
 def time():
-    temp = datetime.datetime.time(datetime.datetime.now())
-    return render_template('time.html', title="Current Time", temp=str(temp))
+    time = datetime.datetime.time(datetime.datetime.now())
+    return render_template('time.html', title="Current Time", temp=str(time))
 
 
 @app.route('/_status', methods=['GET',  'POST'])
 def update_time():
-    temp = datetime.datetime.time(datetime.datetime.now())
-    return jsonify(title="Current Time", temp=str(temp))
+    time = datetime.datetime.time(datetime.datetime.now())
+    return jsonify(title="Current Time", temp=str(time))
+
+
+@app.route('/button')
+def button():
+    global temperature
+    temperature = temperature + 1
+    return render_template('button.html', title="Button Demo", temp=temperature)
--- run.py
+++ run.py
@@ -1,7 +1,10 @@
 #!flask/bin/python
 from app import app
 
-app.run(
-    host='0.0.0.0',
-    debug=True
-)
+
+if __name__ == "__main__":
+    global temperature
+    app.run(
+        host='0.0.0.0',
+        debug=True
+    )
